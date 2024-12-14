a1b58bda545c5a398d0c073ec68c565267ea70d6
Abhinav Barwal <abhibarwal7321@gmail.com>
2024-12-14 11:32:41 +0530

    Add files via upload

---
+++ Instagram Clone/loginpage.js
@@ -0,0 +1,19 @@
+const usrname = document.getElementById("detail")
+const pass = document.getElementById("password")
+const loginbtn = document.querySelector(".login")
+
+loginbtn.addEventListener("click", (event) => {
+    console.log("button was clicked")
+    event.preventDefault();
+    const usrnameValue = usrname.value;
+    const passValue = pass.value;
+    if (usrnameValue === "" || passValue === "") {
+        return;
+    }
+    if ((usrnameValue === "8894853644" || usrnameValue === "abhibarwal7321@gmail.com" || usrnameValue === "barwal_sahbb") && passValue === "Abhinav") {
+        window.location.assign("insta.html")
+    }
+    else {
+        alert("Sorry, your password was incorrect. Please double-check your password.");
+    }
+});
